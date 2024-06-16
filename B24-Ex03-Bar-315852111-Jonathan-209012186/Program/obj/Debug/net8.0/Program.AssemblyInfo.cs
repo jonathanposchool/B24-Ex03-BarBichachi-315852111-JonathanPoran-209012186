b88e6cee4f16f89be2cb4077932e3b9c8787f794
Yonty <jonathanpo@mta.ac.il>
2024-06-16 20:29:58 +0300

    init

--- B24-Ex03-Bar-315852111-Jonathan-209012186/Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ B24-Ex03-Bar-315852111-Jonathan-209012186/Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d179784c6223d3e7a454cf2da7921c7a7075a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
